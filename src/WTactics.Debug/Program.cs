216f6f0b92a65cb9d75c23fe0b55d0cbb833bbd3
Steven Confessore <steven.confessore@gmail.com>
2019-12-03 11:57:47 -0500

    moving service provider to the main library

--- src/WTactics.Debug/Program.cs
+++ src/WTactics.Debug/Program.cs
@@ -1,41 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Threading.Tasks;
-using WTactics.Services;
-using WTactics.Services.Interfaces;
+﻿using System.Threading.Tasks;
 
 namespace WTactics.Debug
 {
     class Program
     {
-        readonly IServiceProvider services;
-
-        Program()
-        {
-            services = ConfigureServices();
-        }
-
         static void Main(string[] args) =>
             new Program().MainAsync().GetAwaiter().GetResult();
 
         async Task MainAsync()
         {
-            services.GetRequiredService<DawningMoon>().Run();
+            new WTactics();
             await Task.Delay(-1);
-        }
-
-        IServiceProvider ConfigureServices()
-        {
-            return new ServiceCollection()
-                .AddSingleton<DawningMoon>()
-                .AddSingleton<IRandomService, RandomService>()
-                .AddSingleton<IContentService, ContentService>()
-                .AddSingleton<ITileService, TileService>()
-                .AddSingleton<IMapService, MapService>()
-                .AddSingleton<IEventService, EventService>()
-                .AddSingleton<EventHandlerService>()
-                .AddSingleton<IRegistrationService, RegistrationService>()
-                .BuildServiceProvider();
         }
     }
 }
